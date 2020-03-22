ba30bb682cd4d4e04d21a0d42207211f0fe262ad
kiwiyuki <yuki.takagi.a@mixi.co.jp>
2020-03-22 23:20:39 +0900

    Use MySQL instead of SQLite3

--- infra/handler.go
+++ infra/handler.go
@@ -1,11 +1,13 @@
 package infra
 
 import (
+	"fmt"
 	"go-todo-list-app/domain/model"
 	"go-todo-list-app/interface/database"
 
 	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/joho/godotenv"
 )
 
 type SQLHandler struct {
@@ -13,11 +15,19 @@ type SQLHandler struct {
 }
 
 func NewSQLHandler() database.SQLHandler {
-	db, err := gorm.Open("sqlite3", ":memory:")
+	env, err := godotenv.Read("/app/.env")
 	if err != nil {
 		panic(err.Error)
 	}
 
+	protocol := "tcp(database:3306)"
+	params := "charset=utf8mb4&parseTime=True"
+	conn := fmt.Sprintf("%s:%s@%s/%s?%s", env["MYSQL_USER"], env["MYSQL_PASSWORD"], protocol, env["MYSQL_DATABASE"], params)
+	db, err := gorm.Open("mysql", conn)
+	if err != nil {
+		panic(err.Error)
+	}
+	db.LogMode(true)
 	db.AutoMigrate(&model.Task{})
 
 	handler := new(SQLHandler)
